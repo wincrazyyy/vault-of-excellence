9d456e110b6041382254e4e1a01c5331f851bd72
Anthony Leung <anthonyleung07@gmail.com>
2026-01-23 20:18:09 +0000

    login refresh

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -14,7 +14,7 @@ export default function ProtectedLayout({
   return (
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+        {/* <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
               <Link href={"/"}>Next.js Supabase Starter</Link>
@@ -30,11 +30,11 @@ export default function ProtectedLayout({
               </Suspense>
             )}
           </div>
-        </nav>
+        </nav> */}
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           {children}
         </div>
-
+{/* 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
           <p>
             Powered by{" "}
@@ -48,7 +48,7 @@ export default function ProtectedLayout({
             </a>
           </p>
           <ThemeSwitcher />
-        </footer>
+        </footer> */}
       </div>
     </main>
   );
