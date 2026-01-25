ab2490c23d24a13a02a58ab0d48cd13adff8e164
Anthony Leung <anthonyleung07@gmail.com>
2026-01-25 09:00:19 +0000

    Update page.tsx

--- app/auth/sign-up-success/page.tsx
+++ app/auth/sign-up-success/page.tsx
@@ -8,14 +8,12 @@ import {
 
 export default function Page() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <div className="flex min-h-[calc(100svh-6rem)] w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
         <div className="flex flex-col gap-6">
           <Card>
             <CardHeader>
-              <CardTitle className="text-2xl">
-                Thank you for signing up!
-              </CardTitle>
+              <CardTitle className="text-2xl">Thank you for signing up!</CardTitle>
               <CardDescription>Check your email to confirm</CardDescription>
             </CardHeader>
             <CardContent>
