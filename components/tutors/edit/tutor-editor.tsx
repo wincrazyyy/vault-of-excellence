6e4f147b1540e5ebfb5bc03273d78abe1ce37a5b
Anthony Leung <anthonyleung07@gmail.com>
2026-02-28 16:19:45 +0000

    Update tutor-editor.tsx

--- components/tutors/edit/tutor-editor.tsx
+++ components/tutors/edit/tutor-editor.tsx
@@ -2,7 +2,6 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { createClient } from "@/lib/supabase/client";
 
