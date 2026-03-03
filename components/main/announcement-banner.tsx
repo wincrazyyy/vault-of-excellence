c3eebc689f05ee4f0afc4d894ecf01c3d1bebfce
Anthony Leung <anthonyleung07@gmail.com>
2026-03-03 14:44:21 +0000

    event announcement link

--- components/main/announcement-banner.tsx
+++ components/main/announcement-banner.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export function AnnouncementBanner() {
   return (
     <div className="w-full bg-violet-600 px-6 py-2 text-center text-sm text-white">
-      🚀 Looking to become a tutor? Join our upcoming webinar!{" "}
-      <Link href="#" className="underline font-bold">
+      🚀 Looking to become a tutor? Join our upcoming event!{" "}
+      <Link href="https://www.instagram.com/p/DVXzpLlE0C0/?igsh=MWwxZGRiZDh3NGk3Mw%3D%3D" className="underline font-bold">
         Learn more
       </Link>
     </div>
