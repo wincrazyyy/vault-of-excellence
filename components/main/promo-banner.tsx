53f0fa6317f4cd9fc92210b0d5c83e2306d82d7d
Anthony Leung <anthonyleung07@gmail.com>
2026-03-05 22:02:24 +0000

    banner image update

--- components/main/promo-banner.tsx
+++ components/main/promo-banner.tsx
@@ -4,7 +4,7 @@ export function PromoBanner() {
   return (
     <div className="mt-12 relative w-full aspect-21/9 sm:aspect-3/1 overflow-hidden rounded-2xl shadow-sm border border-border">
       <Image 
-        src="/banner-image.png" 
+        src="/banner-image.jpg" 
         alt="Promotional Banner" 
         fill 
         className="object-cover" 
